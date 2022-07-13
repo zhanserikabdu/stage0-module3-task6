8672da2de56b86800e2ec85208b9be80167764c6
NANO <File changed>
2022-07-13 23:25:51 +0600

    1 Submission

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+
     public void roundNumber(float numberToBeRounded) {
+
+        int a = Math.round(numberToBeRounded);
+        System.out.println(a);
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000L;
+        int second = (int)first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
